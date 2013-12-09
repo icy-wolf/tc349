77f8dcd25558f572a672fda1d6ccb481d3428a63
icy-wolf <lawre272@msu.edu>
2013-12-08 22:39:38 -0500

    final project progress

--- finalproject/js/main.js
+++ finalproject/js/main.js
@@ -1,1 +1,7 @@
+function animal(){
+alert("To the left is my golden retriever Alexis and to the right is my tabby cat Abby.");
+}
 
+function ghc(){
+alert("This is a picture of me while attending the celebration night party at the Grace Hopper Conference 2013.");
+}
